f0d5ad98c60115e2861af012b43ecbaaaf83cb2c
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-17 13:11:43 -0300

    validate async

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -28,9 +28,10 @@ class LoginScreen extends StatelessWidget {
                 SizedBox(
                   height: 30,
                 ),
-                ChangeNotifierProvider(create:(_) => LoginFormProvider(),
-                child: _LoginForm(),),
-                
+                ChangeNotifierProvider(
+                  create: (_) => LoginFormProvider(),
+                  child: _LoginForm(),
+                ),
               ],
             )),
             SizedBox(
@@ -53,66 +54,79 @@ class _LoginForm extends StatelessWidget {
     final loginForm = Provider.of<LoginFormProvider>(context);
     return Container(
       child: Form(
-        key: loginForm.formKey,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
+          key: loginForm.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: 'noseuqepn@gmail.com',
-                labelText: 'correo electronico',
-                prefixIcon: Icons.alternate_email_outlined),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'noseuqepn@gmail.com',
+                    labelText: 'correo electronico',
+                    prefixIcon: Icons.alternate_email_outlined),
                 onChanged: (value) => loginForm.email = value,
-            validator: (value) {
-              String pattern =
-                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-              RegExp regExp = RegExp(pattern);
-              return regExp.hasMatch(value ?? '')
-                  ? null
-                  : 'El valor ingresado no luce como un correo';
-            },
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-            autocorrect: false,
-            obscureText: true,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: '******',
-                labelText: 'Contraseña',
-                prefixIcon: Icons.lock_clock_outlined),
-                onChanged: (value) => loginForm.password = value,
-            validator: (value) {
-              return (value != null && value.length >= 6)
-                  ? null
-                  : 'La contraseña debe de ser de 6 caracteres';
-            },
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              disabledColor: Colors.grey,
-              elevation: 0,
-              color: Colors.deepPurple,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: Text(
-                  'Ingresar',
-                  style: TextStyle(color: Colors.white),
-                ),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = RegExp(pattern);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El valor ingresado no luce como un correo';
+                },
               ),
-              onPressed: (() {
-                loginForm.isValidForm();
-              }))
-        ],
-      )),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '******',
+                    labelText: 'Contraseña',
+                    prefixIcon: Icons.lock_clock_outlined),
+                onChanged: (value) => loginForm.password = value,
+                validator: (value) {
+                  return (value != null && value.length >= 6)
+                      ? null
+                      : 'La contraseña debe de ser de 6 caracteres';
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  disabledColor: Colors.grey,
+                  elevation: 0,
+                  color: Colors.deepPurple,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    child: Text(
+                      loginForm.isLoading ? 'Espere' : 'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  onPressed: loginForm.isLoading
+                      ? null
+                      : () async {
+                          FocusScope.of(context).unfocus();
+
+                          if (!loginForm.isValidForm()) return;
+
+                          loginForm.isLoading = true;
+
+                          await Future.delayed(Duration(seconds: 2));
+
+                          // TODO: validar si el login es correcto
+                          loginForm.isLoading = false;
+
+                          Navigator.pushReplacementNamed(context, 'home');
+                        }),
+            ],
+          )),
     );
   }
 }
