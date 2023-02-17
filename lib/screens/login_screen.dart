af9c6dd7f061829afbd44e7ec8abd7a02626962b
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-17 12:31:28 -0300

    add regex

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,49 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
 
-
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthBackground(child: SingleChildScrollView(child: Column(
-        children: [
-          SizedBox(height: 100,),
-
-          CardContainer(child: Column(children: [
-            SizedBox(height: 10,),
-            Text('login',style: Theme.of(context).textTheme.bodyMedium,),
-            SizedBox(height: 30,),
-            _LoginForm(),
-          ],)),
-          SizedBox(height: 50,),
-          Text('Crear nueva cuenta'),
-          SizedBox(
+      body: AuthBackground(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            CardContainer(
+                child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'login',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                _LoginForm(),
+              ],
+            )),
+            SizedBox(
               height: 50,
             ),
-
-        ],
-      ),)),
-   );
+            Text('Crear nueva cuenta'),
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
 
-
 class _LoginForm extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Form(child: Column(children: [
-
-        TextFormField(
-          autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecorations.authInputDecoration(hintText: 'noseuqepn@gmail.com', labelText: 'correo electronico',prefixIcon: Icons.alternate_email_outlined),
-        ),
-        SizedBox(height: 30,),
-        TextFormField(
+      child: Form(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: 'noseuqepn@gmail.com',
+                labelText: 'correo electronico',
+                prefixIcon: Icons.alternate_email_outlined),
+            validator: (value) {
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
+              return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'El valor ingresado no luce como un correo';
+            },
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextFormField(
             autocorrect: false,
             obscureText: true,
             keyboardType: TextInputType.emailAddress,
@@ -52,22 +78,31 @@ class _LoginForm extends StatelessWidget {
                 hintText: '******',
                 labelText: 'Contraseña',
                 prefixIcon: Icons.lock_clock_outlined),
+            validator: (value) {
+              return (value != null && value.length >= 6)
+                  ? null
+                  : 'La contraseña debe de ser de 6 caracteres';
+            },
           ),
-          SizedBox(height: 30,),
-
+          SizedBox(
+            height: 30,
+          ),
           MaterialButton(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            disabledColor: Colors.grey,
-            elevation: 0,
-            color: Colors.deepPurple,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 80,vertical: 15),
-              child: Text('Ingresar',style: TextStyle(color: Colors.white),),
-            ),
-            onPressed: null)
-
-
-      ],)),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(
+                  'Ingresar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              onPressed: null)
+        ],
+      )),
     );
   }
 }
