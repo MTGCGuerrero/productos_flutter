8225be1fbf9e9af9c770295899e74ef2bfbf225d
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-18 14:39:31 -0300

    product card part 1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Homescreen'),
-      ),
+      appBar: AppBar(title: Text('Productos')),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: ((context, index) => ProductCard())
+        
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          
+        },
+        child: Icon(Icons.add),),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,7 @@
 
 
+export 'package:products_app/widgets/product_card.dart';
+
 export 'package:products_app/widgets/card_container.dart';
 
 export 'package:products_app/widgets/auth_background.dart';
