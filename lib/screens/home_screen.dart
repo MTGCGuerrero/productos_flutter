48a150872bd68c6b078863c77dfb50e0c0a525f8
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-19 14:45:17 -0300

    product screen part 1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,9 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Productos')),
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: ((context, index) => ProductCard())
+        itemBuilder: ((context, index) => GestureDetector(
+          onTap: () => Navigator.pushNamed(context, 'product'),
+          child: ProductCard()))
         
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,8 +1,9 @@
 
 
+export 'package:products_app/widgets/product_image.dart';
+
 export 'package:products_app/widgets/product_card.dart';
 
 export 'package:products_app/widgets/card_container.dart';
 
 export 'package:products_app/widgets/auth_background.dart';
-
