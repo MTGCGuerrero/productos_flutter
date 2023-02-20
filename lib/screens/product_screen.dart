26b486057d7b6197624003cd54bffad198bb560c
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-20 13:39:06 -0300

    peoduct screen part 2

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,25 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 
 class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+   return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
+        // keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                // ProductImage(url: productService.selectedProduct.picture),
+                Positioned(
+                    top: 60,
+                    left: 20,
+                    child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: Icon(Icons.arrow_back_ios_new,
+                          size: 40, color: Colors.white),
+                    )),
+                Positioned(
+                    top: 60,
+                    right: 20,
+                    child: IconButton(
+                      onPressed: () async {
+                        // final picker = new ImagePicker();
+                        // final PickedFile? pickedFile = await picker.getImage(
+                        //     // source: ImageSource.gallery,
+                        //     source: ImageSource.camera,
+                        //     imageQuality: 100);
 
+                        // if (pickedFile == null) {
+                        //   print('No seleccionó nada');
+                        //   return;
+                        // }
 
-          Stack(
-            children: [
-              ProductImage(),
-            ],
-          ),
-
-
-        ],),
+                        // productService
+                        //     .updateSelectedProductImage(pickedFile.path);
+                      },
+                      icon: Icon(Icons.camera_alt_outlined,
+                          size: 40, color: Colors.white),
+                    ))
+              ],
+            ),
+            _ProductForm(),
+            SizedBox(height: 100),
+          ],
+        ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      // floatingActionButton: FloatingActionButton(
+      //   child: productService.isSaving
+      //       ? CircularProgressIndicator(color: Colors.white)
+      //       : Icon(Icons.save_outlined),
+      //   onPressed: productService.isSaving
+      //       ? null
+      //       : () async {
+      //           if (!productForm.isValidForm()) return;
+
+      //           final String? imageUrl = await productService.uploadImage();
+
+      //           if (imageUrl != null) productForm.product.picture = imageUrl;
+
+      //           await productService.saveOrCreateProduct(productForm.product);
+      //         },
+      // ),
    );
   }
+}
+class _ProductForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          key: productForm.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              TextFormField(
+                initialValue: product.name,
+                onChanged: (value) => product.name = value,
+                validator: (value) {
+                  if (value == null || value.length < 1)
+                    return 'El nombre es obligatorio';
+                },
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'Nombre del producto', labelText: 'Nombre:'),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                initialValue: '${product.price}',
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                onChanged: (value) {
+                  if (double.tryParse(value) == null) {
+                    product.price = 0;
+                  } else {
+                    product.price = double.parse(value);
+                  }
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '\$150', labelText: 'Precio:'),
+              ),
+              SizedBox(height: 30),
+              SwitchListTile.adaptive(
+                  value: product.available,
+                  title: Text('Disponible'),
+                  activeColor: Colors.indigo,
+                  onChanged: productForm.updateAvailability),
+              SizedBox(height: 30)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                offset: Offset(0, 5),
+                blurRadius: 5)
+          ]);
 }
